1842625cd2147c3ee1e61ff4c5c514c10a7539ec
AdamWang518 <dodoga518@gmail.com>
2021-11-08 22:03:17 +0800

    t

--- CPP/HW_04/HW_04.c
+++ CPP/HW_04/HW_04.c
@@ -6,13 +6,27 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX_STRING_LEN 500
 int main()
-{ 
-    char user[8];   
+{
+    FILE *fp;
+    struct utmp user_rec;
+    char user[8];
+    char message[MAX_STRING_LEN];
     printf("the user online:\n");
     system("who");
     printf("The user you want to send message\n");
     scanf("%S", &user);
-    
+    fp = fopen("/var/run/utmp", "r");
+    while(fread(&user_rec, sizeof(struct utmp),1,fp)>0)
+    {
+        if(user==user_rec.ut_user)
+        {
+            printf("The message you want to send\n");
+        }
+    }
+    // printf("The message you want to send\n");
+    // scanf("%S", &message);
+    // printf("%S", message);
     return 0;
 }

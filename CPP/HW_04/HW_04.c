3a7b9c20e6256825a3420e0e40083c64066feba7
AdamWang518 <dodoga518@gmail.com>
2021-11-10 22:01:11 +0800

    HW_04 corps

--- CPP/HW_04/HW_04.c
+++ CPP/HW_04/HW_04.c
@@ -6,13 +6,20 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <utmp.h>
 #define MAX_STRING_LEN 500
 int main()
 {
     FILE *fp;
     struct utmp user_rec;
     char user[8];
+    char *line[15];
+    char *line2=" > /dev/";
+    strcpy(line, line2);
     char message[MAX_STRING_LEN];
+    char *command[MAX_STRING_LEN];
+    char *command2= "echo -e ";
+    strcpy(command, command2);
     printf("the user online:\n");
     system("who");
     printf("The user you want to send message\n");
@@ -20,10 +27,17 @@ int main()
     fp = fopen("/var/run/utmp", "r");
     while(fread(&user_rec, sizeof(struct utmp),1,fp)>0)
     {
-        if(user==user_rec.ut_user)
+        if(user_rec.ut_user==user)
         {
-            printf("The message you want to send\n");
+            printf("Find it");
+            printf("What message would you like to send?");
+            scanf("\"%S\"", &message);
+            strcat(line, user_rec.ut_line);
+            strcat(message,line);
+            strcat(command, message);
+            system(command);
         }
+        //printf("%s\t\t%s\n", user_rec.ut_user, user_rec.ut_line);
     }
     // printf("The message you want to send\n");
     // scanf("%S", &message);

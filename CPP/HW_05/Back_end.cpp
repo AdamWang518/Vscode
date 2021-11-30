ee1eced381fcbe1572e2e9f486907c546598107b
AdamWang518 <dodoga518@gmail.com>
2021-11-30 14:56:31 +0800

    fifo stuck

--- CPP/HW_05/Back_end.cpp
+++ CPP/HW_05/Back_end.cpp
@@ -2,6 +2,10 @@
 #include<string>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+
+#include<fstream>
+#define FIFO_PATH "/myfifo"
 using namespace std;
 class ListNode{
     private:
@@ -23,6 +27,28 @@ class LinkedList{
         void Sort();             // 將整份list依照ID排好(非必要)
 };
 int main(){
-    
+    int ret;
+	int fd;
+	char buffer;
+	int nread;
+	int i;
+	/*建立FIFO*/
+	ret = mkfifo(FIFO_PATH, 0777);
+	/*打開FIFO*/
+	fd = open(FIFO_PATH, O_RDONLY);
+	if(-1 == fd)
+	{
+		printf("error/n");
+		return -1;
+	}
+	while(1)
+	{
+		nread = read(fd, &buffer, 1);
+		if(nread > 0) 
+		{
+			buffer = toupper(buffer);
+			printf("%c", buffer);
+		}
+	}
     return 0;
 }
--- CPP/HW_05/Front_end.cpp
+++ CPP/HW_05/Front_end.cpp
@@ -2,6 +2,9 @@
 #include<string>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#define FIFO_PATH "/myfifo"
 using namespace std;
 int main(){
     int option;
@@ -9,6 +12,13 @@ int main(){
     string Name;
     int ID;
     int Balance;
+    int fd;
+    fd = open(FIFO_PATH, O_WRONLY);
+    if(-1 == fd)
+	{
+        cout << "FIFO ERROR" << endl;
+        return -1;
+	}
     while(n)
     {
         cout << "--------Options--------" << endl;
@@ -20,27 +30,70 @@ int main(){
         cout << "-----------------------" << endl;
         cout << "Please select an option" << endl;
         cin >> option;
-        switch(option){
-            case 1:
-                cout << "Enter the name:" << endl;
-                cin >> Name;
-                cout << "Enter the ID:" << endl;
-                cin >> ID;
-                cout << "Enter the balance" << endl;
-                cin >> Balance;
-                break;
-            case 2:
-                cout << "Enter the ID to search" << endl;
-                break;
-            case 3:
-                cout << "Enter the ID to Delete" << endl;
-                break;
-            case 4:
-                break;
-            case 5:
-                n = 0;
-                break;
+        std::string tmp = std::to_string(option);
+        char const *optionChar = tmp.c_str();
+        write(fd, &optionChar, 1);
+        if(option==1)
+        {
+            cout << "Enter the name:" << endl;
+            cin >> Name;
+            cout << "Enter the ID:" << endl;
+            cin >> ID;
+            cout << "Enter the balance" << endl;
+            cin >> Balance;
         }
+        else if(option==2)
+        {
+            cout << "Enter the ID to search" << endl;
+            cin >> ID;
+            string tmp = to_string(option);
+            char const *IDChar = tmp.c_str();
+            write(fd, &IDChar, 1);
+        }
+        else if(option==3)
+        {
+            cout << "Enter the ID to Delete" << endl;
+            cin >> ID;
+            break;
+        }
+        else if(option==4)
+        {
+
+        }
+        else if(option==5)
+        {
+            n = 0;
+        }
+        else
+        {
+            cout << "Please Enter again" << endl;
+        }
+        // switch(option){
+        //     case 1:
+        //         cout << "Enter the name:" << endl;
+        //         cin >> Name;
+        //         cout << "Enter the ID:" << endl;
+        //         cin >> ID;
+        //         cout << "Enter the balance" << endl;
+        //         cin >> Balance;
+        //         break;
+        //     case 2:
+        //         cout << "Enter the ID to search" << endl;
+        //         cin >> ID;
+        //         string tmp = to_string(option);
+        //         char const *IDChar = tmp.c_str();
+        //         write(fd, &IDChar, 1);
+        //         break;
+        //     case 3:
+        //         cout << "Enter the ID to Delete" << endl;
+        //         cin >> ID;
+        //         break;
+        //     case 4:
+        //         break;
+        //     case 5:
+        //         n = 0;
+        //         break;
+        // }
     }
     return 0;
 }

893c24fe746fdacf9b368a39f712f87313a775b4
AdamWang518 <dodoga518@gmail.com>
2021-12-03 13:58:01 +0800

    Great breakthrough

--- CPP/HW_05/Back_end.cpp
+++ CPP/HW_05/Back_end.cpp
@@ -1,50 +1,63 @@
-#include<iostream>
-#include<string>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include<fstream>
-#include<unistd.h>
-#include<cstring>
+#include <cstring>
 #include <errno.h>
+#include <fcntl.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include<stdio.h>
 #define FIFO_PATH "fifo"
 #define MY_FIFO_PATH "myfifo"
 using namespace std;
-class ListNode{
-    private:
-        char Name[50];
-        int ID;
-        int Balance;
-        ListNode *Next=NULL;
+class ListNode
+{
+private:
+    char Name[50];
+    int ID;
+    int Balance;
+    ListNode *Next = NULL;
     friend class LinkedList;
 };
-class LinkedList{
-    private:
-        ListNode *first;            // list的header，裡面不放東西，
-    public:
-        void Start();
-        void PrintList();      // 印出list的所有資料
-        int Insert(char Name[50], int ID, int Balance);         // 在list新增一個node
-        int Search(int ID);         //找出list中特定ID的Node
-        int Delete(int ID);         // 刪除list中特定ID的Node
-        void BackUp(int signum);              //在被SIGINT時備份所有內容
-        void ReBuild();         //以備份重建List
-        void Clear();           //將整份list刪除(非必要)
-        void Sort();             // 將整份list依照ID排好(非必要)
-};
+class LinkedList
+{
+private:
+    ListNode *first; // list的header，裡面不放東西，
+    int number = 0;  //計有幾筆資料
 
-void LinkedList::Start(){
+public:
+    void Start();
+    void PrintList(int max_len);                               // 印出list的所有資料
+    int Insert(char Name[50], int ID, int Balance); // 在list新增一個node
+    int Search(int ID, char *Array,int max_len);                //找出list中特定ID的Node
+    int Delete(int ID);                             // 刪除list中特定ID的Node
+    void BackUp(int signum);                        //在被SIGINT時備份所有內容
+    void ReBuild();                                 //以備份重建List
+    void Clear();                                   //將整份list刪除(非必要)
+    void Sort();                                    // 將整份list依照ID排好(非必要)
+    int getNumber();
+};
+int LinkedList::getNumber()
+{
+    return number;
+}
+void LinkedList::Start()
+{
     first = new ListNode();
     first->Next = NULL;
 }
-void LinkedList::PrintList(){
-    ListNode *current = first->Next; 
-    if (current == NULL) {                      // 如果current指向NULL, 表示list沒有資料
+void LinkedList::PrintList(int max_len)
+{
+    ListNode *current = first->Next;
+    if (current == NULL)
+    { // 如果current指向NULL, 表示list沒有資料
         cout << "List is empty.\n";
         return;
     }
     // 用pointer *current在list中移動
-    while (current != NULL) {                 // Traversal
+    while (current != NULL)
+    { // Traversal
         cout << "-----------------------" << endl;
         cout << "Name:" << current->Name << endl;
         cout << "ID:" << current->ID << endl;
@@ -54,15 +67,24 @@ void LinkedList::PrintList(){
     }
     cout << endl;
 }
-int LinkedList::Insert(char Name[50], int ID, int Balance){
-    ListNode *current=first;
-    while (current->Next != NULL) {                 // Traversal
-        current = current->Next;//讓current走到list的尾端，過程中順便檢查ID是否重複
-        if(current->ID==ID)
+int LinkedList::Insert(char Name[50], int ID, int Balance)
+{
+    ListNode *current = first;
+    number = 0;
+    while (current->Next != NULL)
+    {                            // Traversal
+        current = current->Next; //讓current走到list的尾端
+        number++;
+    }
+    current = first;
+    while (current->Next != NULL)
+    {                            // Traversal
+        current = current->Next; //讓current走到list的尾端，過程中順便檢查ID是否重複
+        if (current->ID == ID)
         {
             cout << "The ID already exist" << endl;
             return 0;
-        }//如果已經存在則讓使用者再次輸入        
+        } //如果已經存在則讓使用者再次輸入
     }
     ListNode *node = new ListNode; //創造新node
     strcpy(node->Name, Name);
@@ -74,35 +96,35 @@ int LinkedList::Insert(char Name[50], int ID, int Balance){
     cout << "Insert Success" << endl;
     return 1;
 }
-int LinkedList::Search(int ID)
-{
+int LinkedList::Search(int ID,char *Array,int max_len)
+{//不要分類直接回傳char 陣列
     bool Flag = false;
     ListNode *current = first->Next;
-    if (current == NULL) {                      // 如果current指向NULL, 表示list沒有資料
-        cout << "List is empty.\n";
-        return 0;
+    if (current == NULL)
+    { // 如果current指向NULL, 表示list沒有資料
+        int length = snprintf(Array,max_len,  "List is empty.\n");
+        return length;
     }
-    while (current != NULL) {                 // Traversal
-        if(current->ID==ID)
+    while (current != NULL)
+    { // Traversal
+        if (current->ID == ID)
         {
-            cout << "-----------------------" << endl;
-            cout << "Name:" << current->Name << endl;
-            cout << "ID:" << current->ID << endl;
-            cout << "Balance:" << current->Balance << endl;
-            cout << "-----------------------" << endl;
-            Flag = true;
+            int length = snprintf(Array,max_len, "Name:%s\nID:%d\nBalnce:%d", current->Name, current->ID, current->Balance);
+            // cout << "-----------------------" << endl;
+            // cout << "Name:" << current->Name << endl;
+            // cout << "ID:" << current->ID << endl;
+            // cout << "Balance:" << current->Balance << endl;
+            // cout << "-----------------------" << endl;
+            return length;
         }
         current = current->Next;
     }
-    if(Flag==false)
+    if (Flag == false)
     {
+        int length = snprintf(Array, max_len, "Thist ID does Not Exist");
         cout << "Thist ID does Not Exist" << endl;
-        return 2;
+        return length;
     }
-    else{
-        return 1;
-    }
-    cout << endl;
 }
 int LinkedList::Delete(int ID)
 {
@@ -110,16 +132,19 @@ int LinkedList::Delete(int ID)
     ListNode *forward = first;
     ListNode *current = first->Next;
     ListNode *backward = current->Next;
-    if (current == NULL) {                      // 如果current指向NULL, 表示list沒有資料
+    if (current == NULL)
+    { // 如果current指向NULL, 表示list沒有資料
         cout << "List is empty.\n";
         return 0;
     }
-    while (current != NULL) {                 // Traversal
-        if(current->ID==ID)
+    while (current != NULL)
+    { // Traversal
+        if (current->ID == ID)
         {
             forward->Next = backward;
             delete current;
             cout << "delete success" << endl;
+            number--;
             Flag = true;
             return 1;
         } //將該node刪除並銜接前後的node
@@ -127,7 +152,7 @@ int LinkedList::Delete(int ID)
         current = current->Next;
         backward = current->Next;
     }
-    if(Flag==false)
+    if (Flag == false)
     {
         cout << "Thist ID does Not Exist" << endl;
         return 2;
@@ -135,7 +160,8 @@ int LinkedList::Delete(int ID)
     cout << endl;
 }
 
-int main(){
+int main()
+{
     LinkedList list;
     list.Start();
     int option;
@@ -144,79 +170,89 @@ int main(){
     char NameChar[50];
     int ID;
     int Balance;
-	int ret;
+    int ret;
     int wet;
-    int fd;
-    int wfd;
+    int back;
+    int front;
     int flag;
     /*建立FIFO*/
-     if(mkfifo(FIFO_PATH, 0777)==-1||mkfifo(MY_FIFO_PATH, 0777)==-1){
-        if(errno!=EEXIST){
+    if (mkfifo("back", 0777) == -1 || mkfifo("front", 0777) == -1)
+    {
+        if (errno != EEXIST)
+        {
             printf("建立FIFO檔發生錯誤，程式中止。\n");
             return 2;
         }
     }
-    wfd = open(MY_FIFO_PATH, O_WRONLY);
-    fd = open(FIFO_PATH, O_RDONLY);
-	/*打開FIFO*/
-	while(1)
-	{
-        
-	    if(-1 == fd)
-	    {
-            cout << "Error" << endl;
-            return -1;
-	    }
-        if(-1==wfd)
+    back = open("back", O_RDONLY);
+    front = open("front", O_WRONLY);
+    if (back == -1 || front == -1)
+    {
+        cout << "開檔錯誤。\n";
+    }
+    /*打開FIFO*/
+    while (1)
+    {
+
+        if (back == -1)
         {
             cout << "Error" << endl;
             return -1;
         }
-        read(fd, &option, sizeof(option));
-        cout << option << endl;
-        switch(option){
-            case 1:
-                cout << "Enter the name:" << endl;
-                //cin >> Name;
-                read(fd, &NameChar, sizeof(NameChar));
-                //cout << NameChar << endl;
-                cout << "Enter the ID:" << endl;
-                //cin >> ID;
-                read(fd, &ID, sizeof(ID));
-                cout << "Enter the balance" << endl;
-                //cout << ID << endl;
-                //cin >> Balance;
-                read(fd, &Balance, sizeof(Balance));
-                //cout << Balance << endl;
-                flag=list.Insert(NameChar, ID, Balance);
-                //write(wfd, &flag, sizeof(flag));
-                break;
-            case 2:
-                cout << "Enter the ID to search" << endl;
-                //cin >> ID;
-                read(fd, &ID, sizeof(ID));
-                flag=list.Search(ID);
-                //write(wfd, &flag, sizeof(flag));
-                break;
-            case 3:
-                cout << "Enter the ID to Delete" << endl;
-                //cin >> ID;
-                read(fd, &ID, sizeof(ID));
-                flag=list.Delete(ID);
-                //write(wfd, &flag, sizeof(flag));
-                break;
-            case 4:
-                list.PrintList();
-                break;
-            case 5:
-                cout << "Front end closed" << endl;
-                break;
-            default:
-                cout << "Please Enter again" << endl;
-                break;
+        if (front == -1)
+        {
+            cout << "Error" << endl;
+            return -1;
         }
-        close(fd);
-        close(wfd);
+        read(back, &option, sizeof(int));
+        if(option==1)
+        {
+            // cout << "Enter the name:" << endl;
+            //cin >> Name;
+            read(back, &NameChar, sizeof(NameChar));
+            //cout << NameChar << endl;
+            //cout << "Enter the ID:" << endl;
+            //cin >> ID;
+            read(back, &ID, sizeof(ID));
+            //cout << "Enter the balance" << endl;
+            //cout << ID << endl;
+            //cin >> Balance;
+            read(back, &Balance, sizeof(Balance));
+            //cout << Balance << endl;
+            flag = list.Insert(NameChar, ID, Balance);
+            write(front, &flag, sizeof(flag));
+        }
+        else if(option==2)
+        {
+           // cout << "Enter the ID to search" << endl;
+            read(back, &ID, sizeof(ID));
+            char output[100]="";
+            int length = list.Search(ID, output, 100);
+            write(front, &length, sizeof(int));
+            write(front, &output, length);
+        }
+        else if(option==3)
+        {
+            //cout << "Enter the ID to Delete" << endl;
+            //cin >> ID;
+            read(back, &ID, sizeof(ID));
+            flag = list.Delete(ID);
+            write(front, &flag, sizeof(flag));
+        }
+        else if(option==4)
+        {
+            list.PrintList(100);
+        }
+        else if(option==5)
+        {
+            cout << "Front end closed" << endl;
+            break;
+        }
+        
+        option = 0;
     }
+    close(back);
+    close(front);
+
     return 0;
 }
--- CPP/HW_05/Front_end.cpp
+++ CPP/HW_05/Front_end.cpp
@@ -1,39 +1,39 @@
-#include<iostream>
-#include<string>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <iostream>
 #include <stdio.h>
-#include<unistd.h>
-
+#include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <algorithm>
 #define FIFO_PATH "fifo"
 #define MY_FIFO_PATH "myfifo"
 
 using namespace std;
-int main(){
-    int fd;
-    int rfd;
+int main()
+{
+    int back;
+    int front;
     int ret;
-	char c;
+    char c;
     int flag;
     int option;
     bool n = 1;
-    
+    char FIFO[] = "fifo";
+    char MYFIFO[] = "myfifo";
     int ID;
     int Balance;
-    fd = open(FIFO_PATH, O_WRONLY);//stuck here
-    rfd = open(MY_FIFO_PATH, O_RDONLY);//or here
-    while(n)
+    back = open("back", O_WRONLY);   //stuck here
+    front = open("front", O_RDONLY); //or here
+    while (n)
     {
         char Namechar[50];
-        
-        
-        
-        if(-1 == fd)
-	    {
-		    printf("ERROR/n");
-		    return -1;
-	    }
+
+        if (-1 == back)
+        {
+            printf("ERROR/n");
+            return -1;
+        }
         cout << "--------Options--------" << endl;
         cout << "(1) insert data record" << endl;
         cout << "(2) Search data record" << endl;
@@ -43,83 +43,79 @@ int main(){
         cout << "-----------------------" << endl;
         cout << "Please select an option" << endl;
         cin >> option;
-        write(fd, &option, sizeof(option));
-        switch(option){
-            case 1:
-                cout << "Enter the name:" << endl;
-                cin >> Namechar;
-                //char *NameChar = Name.c_str();
-                write(fd, &Namechar, sizeof(Namechar));
-                cout << "Enter the ID:" << endl;
-                cin >> ID;
-                write(fd, &ID, sizeof(ID));
-                cout << "Enter the balance" << endl;
-                cin >> Balance;
-                write(fd, &Balance, sizeof(Balance));
-                // read(rfd, &flag, sizeof(flag));
-                // if(flag==0)
-                // {
-                //     cout << "ID already exist" << endl;
-                // }
-                // else if(flag==1)
-                // {
-                //     cout << "Inser scuccess" << endl;
-                // }
-                break;
-            case 2:
-                cout << "Enter the ID to search" << endl;
-                cin >> ID;
-                write(fd, &ID, sizeof(ID));
-                // read(rfd, &flag, sizeof(flag));
-                // if(flag==0)
-                // {
-                //     cout << "List is empty" << endl;
-                // }
-                // else if(flag==1)
-                // {
-                //     read(rfd, &Namechar, sizeof(Namechar));
-                //     read(rfd, &ID, sizeof(ID));
-                //     read(rfd, &Balance, sizeof(Balance));
-                //     cout << "-----------------------" << endl;
-                //     cout << "Name:" << Namechar << endl;
-                //     cout << "ID:" << ID << endl;
-                //     cout << "Balance:" << Balance << endl;
-                //     cout << "-----------------------" << endl;
-                // }
-                // else if(flag==2)
-                // {
-                //     cout << "Thist ID does Not Exist" << endl;
-                // }
-                break;
-            case 3:
-                cout << "Enter the ID to Delete" << endl;
-                cin >> ID;
-                write(fd, &ID, sizeof(ID));
-                // read(rfd, &flag, sizeof(flag));
-                // if(flag==0)
-                // {
-                //     cout << "List is empty.\n";
-                // }
-                // else if(flag==1)
-                // {
-                //      cout << "delete success" << endl;
-                // }
-                // else if(flag==2)
-                // {
-                //     cout << "Thist ID does Not Exist" << endl;
-                // }
-                break;
-            case 4:
-                break;
-            case 5:
-                n = 0;
-                break;
-            default:
-                cout << "Please Enter again" << endl;
-                break;
+        if (option <= 0 || option > 5)
+        {
+            cout << "wrong number" << endl;
+            continue;
         }
-        close(fd);
-        close(rfd);
+        write(back, &option, sizeof(option));
+        if(option==1)
+        {
+            cout << "Enter the name:" << endl;
+            cin >> Namechar;
+            write(back, &Namechar, sizeof(Namechar));
+            cout << "Enter the ID:" << endl;
+            cin >> ID;
+            write(back, &ID, sizeof(ID));
+            cout << "Enter the balance" << endl;
+            cin >> Balance;
+            write(back, &Balance, sizeof(Balance));
+            read(front, &flag, sizeof(flag));
+            if (flag == 0)
+            {
+                cout << "ID already exist" << endl;
+            }
+            else if (flag == 1)
+            {
+                cout << "Inser scuccess" << endl;
+            }
+        }
+        else if(option==2)
+        {
+            cout << "Enter the ID to search" << endl;
+            cin >> ID;
+            write(back, &ID, sizeof(ID));
+            //read(front, &flag, sizeof(flag));
+            int length;
+            char Pack[100] = "";
+            //收一包char陣列就好
+            // 先Read他的length
+            // 再依照length去收char陣列
+            read(front, &length, sizeof(int));
+            read(front, &Pack, length);
+            cout << "-----------------------" << endl;
+            cout << Pack << endl;
+            cout << "-----------------------" << endl;
+        }
+        else if(option==3)
+        {
+            cout << "Enter the ID to Delete" << endl;
+            cin >> ID;
+            write(back, &ID, sizeof(ID));
+            read(front, &flag, sizeof(flag));
+            if (flag == 0)
+            {
+                cout << "List is empty.\n";
+            }
+            else if (flag == 1)
+            {
+                cout << "delete success" << endl;
+            }
+            else if (flag == 2)
+            {
+                cout << "Thist ID does Not Exist" << endl;
+            }
+        }
+        else if(option==4)
+        {
+
+        }
+        else if(option==5)
+        {
+            n = 0;
+        }
     }
+    close(back);
+    close(front);
     return 0;
 }

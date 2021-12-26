b1d1944a51db595000bfcec2a1a494e98e7b240e
AdamWang518 <dodoga518@gmail.com>
2021-12-26 15:49:24 +0800

    cd success

---
+++ CPP/Final/project.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define TOKEN_BUFSIZE 64
+#define MAX_DIR_NAME 2048
+#define LSH_TOK_DELIM " \t\r\n\a"//由於getline會留下一個換行字符，因此除將字串移除空格外也要移除換行符號
+int shell_build_in_cmd(char **args){
+    if(strcmp(args[0],"exit")==0)
+    {
+        exit(0);
+    }
+    else if(strcmp(args[0],"cd")==0)
+    {
+        if(chdir(args[1]))
+        {
+            printf("cd%s:no such directory\n", args[1]);
+        }
+        return 1;
+    }//用來cd
+    else if(strcmp(args[0],"pwd")==0)
+    {
+        char buffer[MAX_DIR_NAME]={0};
+        printf("%s\n", getcwd(buffer,sizeof(buffer)));
+        return 1;
+    }//用來確認目前所在的檔案夾
+    else
+    {
+        return 0;
+    }
+}
+char *shell_read_line()
+{
+    char *line = NULL;
+    ssize_t bufsize = 0; // 利用 getline 來自動分配緩衝區，將整串輸入放入到line中
+    getline(&line, &bufsize, stdin);
+    return line;
+};
+char **shell_split_line(char *line){
+    int bufsize = TOKEN_BUFSIZE, position = 0;
+    char **commands = malloc(bufsize * sizeof(char*));//分配記憶體用以分析以空格分割的輸入，先假設有64個區段
+    char *command;
+    if (!commands) {
+        fprintf(stderr, "shell allocation error\n");//若指令為空則停止分割
+        exit(EXIT_FAILURE);
+    }
+    command = strtok(line, LSH_TOK_DELIM);//利用strtok分離出指令的第一個詞，如cd final中的cd
+    while (command != NULL) {
+        commands[position] = command;//在commands[postion]儲存指令
+        position++;
+        if (position >= bufsize) {//如果片段太長，超過BUFFSIZE就繼續分配更多記憶體給commands
+            bufsize += TOKEN_BUFSIZE;
+            commands = realloc(commands, bufsize * sizeof(char*));//利用realloc在保留內容物的情況下增加記憶體空間
+            if (!commands) {
+                fprintf(stderr, "shell allocation error\n");//若command分配後反而為null則終止程式
+                exit(EXIT_FAILURE);
+            }
+        }
+        command = strtok(NULL, LSH_TOK_DELIM);//繼續分割字串的剩餘部分由於getline會留下一個換行字符，因此除將字串移除空格外也要移除換行符號
+    }
+    commands[position] = NULL;//最後一位設成NULL
+    return commands;
+};
+int shell_execute(char **args)
+{
+    if(shell_build_in_cmd(args))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+};
+void shell_loop()
+{
+    char *line;
+    char **args;
+    int status = 1;
+    while (status)
+    {
+        printf("B0829039's Shell> ");
+        line = shell_read_line();//接收輸入的字串
+        args = shell_split_line(line);//分割輸入的字串
+        status = shell_execute(args);//執行指令
+        free(line);
+        free(args);//回收記憶體
+    }
+};
+int main()
+{
+    shell_loop();
+    return 0;
+}

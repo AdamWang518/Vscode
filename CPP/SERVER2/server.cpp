5146f068d802152ea5deb173cb7f2b868eda05a4
AdamWang518 <dodoga518@gmail.com>
2022-01-09 13:11:40 +0800

    wind

--- CPP/SERVER2/server.cpp
+++ CPP/SERVER2/server.cpp
@@ -71,14 +71,27 @@ int NOTFOND(int socketfd)
 void parse_request(int fd,char *sizeRequest,Objective& Obj)
 {
     string reqMethod = "", target = "",version = "";
-    char sizeResource[4096];
+    char* range;
     char *request;
+    if((range = strstr(sizeRequest, "Range: bytes=")) == NULL){
+        cout << "no range in this request." << endl;
+        Obj.hasRange = false;
+        Obj.start = 0;
+    }
+    else{
+        cout << "find out range in this request." << endl;
+        range = range + 13;
+        range = strtok(range, "-");
+        Obj.hasRange = true;
+        string start = range;
+        Obj.start = stoi(range);
+        cout << "start at: " << Obj.start << endl;
+    }
     request = strtok(sizeRequest, " ");//擷取使用的method
     reqMethod += request;
     cout << reqMethod << endl;
     request = strtok(NULL, " ");    //擷取檔名
     if(request == NULL){    //沒有要求，格式錯誤
-        cout << "400 BAD REQUEST" << endl;
         BADREQUEST(fd);
     }
     request = request + 1; // 跳過第一個斜槓，取得路徑與檔名
@@ -86,7 +99,6 @@ void parse_request(int fd,char *sizeRequest,Objective& Obj)
     cout << target << endl;
     request = strtok(NULL, " \r\n");
     if(request == NULL){//沒有版本，格式錯誤
-        cout << "400 BAD REQUEST." << endl;
         BADREQUEST(fd);
     }
     version += request;
@@ -106,13 +118,109 @@ void handle__request(int fd)
         cout << "a connection was found.\n";
         sRecv=read(fd, buffer, sizeof(buffer));
         cout << buffer << endl;
-        break;
+        parse_request(fd, buffer, obj);
+        if(strcmp(obj.method, "GET")){
+            cout << "wrong method" << endl;
+            BADREQUEST(fd);
+        }
+        if(strcmp(obj.version,"HTTP/1.1"))
+        {
+            cout << "wrong version" << endl;
+            BADREQUEST(fd);
+        }
+        char contentType[30];
+        if(strstr(obj.filename, ".mp4") != NULL){
+            cout << "A Video request" << endl;
+            strcpy(contentType, "video/mp4");
+        }
+        else if(strstr(obj.filename, ".html") != NULL){
+            cout << "A Text request" << endl;
+            strcpy(contentType, "text/html");
+        }
+        char Resource[4096]={0};
+        if(strcmp(contentType, "video/mp4") == 0)
+        {
+            FILE *reader = fopen(obj.filename, "rb+");//讀取影片檔案
+            if(reader == NULL){
+                cout << obj.filename << ":does not exist" << endl;
+                NOTFOND(fd);
+                exit(1);     
+            }
+            else
+            {
+                cout << obj.filename << ":does  exist" << endl;
+                fseek(reader, 0L, SEEK_END);
+                int fileLength = ftell(reader);
+                fseek(reader, 0, SEEK_SET);
+                if(!obj.hasRange){  // 第一次先送header跟range
+                    cout << "200 OK" << endl;
+                    snprintf(Resource, 4096, "HTTP/1.1 200 OK\nContent-Type: %s\nContent-Length: %d\nAccept-Ranges: bytes\n\n", contentType, fileLength);
+                    send(fd, Resource, strlen(Resource), MSG_NOSIGNAL);
+                }
+                else//以片段傳送後續影片
+                {
+                    long contentLeft = fileLength - obj.start;
+                    int fragSize = 1024*64;
+                    int fragNum = contentLeft / fragSize;
+                    if(contentLeft % fragSize != 0)
+                    {
+                        fragNum++;
+                    }
+                    for(int i = 0; i < fragNum; i++)
+                    {
+                        if(i+1 == fragNum)
+                        {
+                            fragSize = fileLength - obj.start;
+                        }
+                        snprintf(Resource, 4096, "HTTP/1.1 206 Partial Content\nContent-Type: %s\nContent-Length: %d\nContent-Range: bytes %d-%d/%d\nAccept-Ranges: bytes\n\n",contentType, fragSize, obj.start, obj.start+fragSize-1, fileLength);
+                        send(fd, Resource, strlen(Resource), MSG_NOSIGNAL);
+                        char sizeBuffer[fragSize]={0};
+                        fseek(reader, obj.start, SEEK_SET);
+                        if(fread(sizeBuffer, 1, fragSize, reader) == 0){
+                            cout << "read video file error." << endl;
+                            exit(1);
+                        }
+                        else
+                        {
+                            send(fd, sizeBuffer, sizeof(sizeBuffer), MSG_NOSIGNAL);
+                            obj.start += fragSize;
+                            memset(sizeBuffer, 0, sizeof(sizeBuffer));
+                        }
+                    }
+                    cout << "Transfer video done" << endl;
+                }
+            }
+        }
+        else
+        {
+            FILE *reader = fopen(obj.filename, "r");
+            if(reader == NULL){
+                cout << obj.filename << ":does not exist" << endl;
+                NOTFOND(fd);
+                exit(1);     
+            }
+            else
+            {
+                cout << obj.filename << ":does  exist" << endl;
+                fseek(reader, 0L, SEEK_END);
+                int fileLength = ftell(reader);
+                fseek(reader, 0, SEEK_SET);
+                
+                snprintf(Resource, 4096, "HTTP/1.1 200 OK\nContent-Type: %s\nContent-Length: %d\nAccept-Ranges: bytes\n\n", contentType, fileLength);
+                send(fd, Resource, strlen(Resource), MSG_NOSIGNAL);
+                cout << "header 200 OK" << endl;
+                char siezBuffer[fileLength]={0};
+                if(fread(siezBuffer, 1, fileLength, reader) == 0){
+                    cout << "error.\n";
+                    exit(1);
+                }
+                send(fd, siezBuffer, sizeof(siezBuffer), 0);
+                cout << "sending body\n";
+            }
+        }
     }
-    parse_request(fd, buffer, obj);
-    if(obj.method != "GET" || obj.version != "HTTP/1.1"){
-        BADREQUEST(fd);
-    }
-    NOTFOND(fd);
+    
+    BADREQUEST(fd);
 }
 int main()
 {
@@ -147,7 +255,7 @@ int main()
     if ((listenfd=socket(AF_INET, SOCK_STREAM,0))<0)
     {
         cout<<"Fail to create a socket.\n";
-        exit(3);
+        exit(1);
     }
     else
     {
@@ -159,7 +267,7 @@ int main()
     if (bind(listenfd, (struct sockaddr *)&serv_addr,sizeof(serv_addr))<0)
     {
         cout << "Bind Fail.\n";
-        exit(3);
+        exit(1);
     }
     else
     {
@@ -168,7 +276,7 @@ int main()
     if (listen(listenfd,64)<0)
     {
         cout << "Listen Fail.\n";
-        exit(3);
+        exit(1);
     }
     else
     {

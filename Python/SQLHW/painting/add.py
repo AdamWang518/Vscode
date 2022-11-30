da464da6f57fc286377da2ed9a0e309c528bf846
AdamWang518 <dodoga518@gmail.com>
2022-11-30 10:59:57 +0800

    error solve

---
+++ Python/SQLHW/painting/add.py
@@ -0,0 +1,37 @@
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+f = open("D:\\Vscode\\Python\\SQLHW\\painting\\problem.text", "r", encoding='utf8')
+pageList = []
+line = f.readline()
+while line:
+    pageList.append(line)
+    line = f.readline()
+f.close()
+f = open("D:\\Vscode\\Python\\SQLHW\\painting\\errors.json", "w", encoding='utf8')
+infoDict=dict()
+infoDict["inFo"]=[]
+for page in pageList:
+    driver.get(page)
+    drawDict = dict()
+    try:
+        drawDict["imageLink"] = str(driver.find_element(
+            By.CSS_SELECTOR, '#title > div.col-md-5.col-lg-6 > img').get_attribute("src")).replace("sthumb_images", "popup_images")
+        drawDict["drawName"] = driver.find_element(
+            By.CSS_SELECTOR, '#title > div.col-md-5.col-lg-6 > img').get_attribute("alt")
+        drawDict["artist"] = driver.find_element(
+            By.CSS_SELECTOR, "#productsInfo > ul > li.d-flex > span").text
+        drawDict["year"] = str(driver.find_element(
+            By.CSS_SELECTOR, "#productsInfo > ul > li:nth-child(3)").text).replace("年\u3000\u3000份", "")
+        drawDict["Attribute"] = driver.find_element(
+            By.CSS_SELECTOR, "#productsInfo > ul > li:nth-child(5)").text.replace("原作材質", "")
+        infoDict["inFo"].append(drawDict) 
+    except:
+        print(page,end="")
+        # 若有特定頁面有問題則將其吐出
+json.dump(infoDict, f, ensure_ascii=False)
+f.close()

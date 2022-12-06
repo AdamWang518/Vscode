23fc2a1008e19b40414fcb7d0bada0e0c3bb320a
AdamWang518 <dodoga518@gmail.com>
2022-12-06 16:23:37 +0800

    TRIMMED

--- Python/ESJ-download/ESJ_PureSelenium.py
+++ Python/ESJ-download/ESJ_PureSelenium.py
@@ -17,13 +17,13 @@ driver.find_element(By.XPATH,'/html/body/div[3]/section/div/div[1]/form/div[1]/i
 driver.find_element(By.XPATH,'/html/body/div[3]/section/div/div[1]/form/div[2]/input').send_keys('20010518')
 driver.find_element(By.XPATH,'/html/body/div[3]/section/div/div[1]/form/div[4]/a').click()
 time.sleep(5)
-driver.get("https://www.esjzone.cc/detail/1659697471.html")#要爬的小說連結
+driver.get("https://www.esjzone.cc/detail/1576573703.html")#要爬的小說連結
 # driver.find_element(By.XPATH,'//*[@id="chapterList"]/details[1]/summary/strong').click()
 # driver.find_element(By.XPATH,'//*[@id="chapterList"]/details[2]/summary/strong').click()
 
 name=driver.find_element(By.CSS_SELECTOR,'body > div.offcanvas-wrapper > section > div > div.col-xl-9.col-lg-8.p-r-30 > div:nth-child(1) > div.col-md-9.book-detail > h2').text
-path = 'D:\\MEGA\\小說\\ESJ爬蟲\\'+'『男女比1比30』世界的黑一點偶像'+'.txt'#要爬的小說名稱
-# path = 'D:\\MEGA\\小說\\ESJ爬蟲\\'+name+'.txt'
+# path = 'D:\\MEGA\\小說\\ESJ爬蟲\\'+'『男女比1比30』世界的黑一點偶像'+'.txt'#要爬的小說名稱
+path = 'D:\\MEGA\\小說\\ESJ爬蟲\\'+name+'.txt'
 f = open(path, 'w',encoding='utf8')
 Details=[]
 try:
